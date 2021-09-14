ebf34a66f97cf8e7b1878a47372be7de1146d25d
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-09-14 14:19:07 +0300

    refactor: update server.ts

--- server/server.ts
+++ server/server.ts
@@ -1,38 +1,37 @@
 import {Response, Request, NextFunction} from "express";
 
-const swaggerUI = require("swagger-ui-express")
-const path = require('path')
-const YAML = require('yamljs')
+const swaggerUI = require("swagger-ui-express");
+const path = require('path');
+const YAML = require('yamljs');
 
 let fileUpload = require('express-fileupload');
 const express = require('express');
 let bodyParser = require('body-parser');
 const config = require('config');
 
-const auth = require('./routes/auth')
-const gallery = require('./routes/gallery')
-const home = require('./routes/home')
+const auth = require('./routes/auth');
+const gallery = require('./routes/gallery');
+const home = require('./routes/home');
 const logger = require('./logger/logger');
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, './docs/openapi/api.yml'));
 
 app.use(bodyParser.json(), (request: Request, response: Response, next: NextFunction) => {
-    console.log(request.url)
+    requestLogging(request);
 
-    requestLogging(request)
     if (request.method == 'POST' && request.query.page || request.method == 'GET' && request.query.page) {
-        checkToken(request, next, response)
-        next()
+        checkToken(request, next, response);
+        next();
     } else {
         next();
     }
 });
 
-app.use("/auth", auth)
-app.use("/", home)
-app.use("/gallery", gallery)
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+app.use("/auth", auth);
+app.use("/", home);
+app.use("/gallery", gallery);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use(fileUpload({}));
 app.use(express.static(config.get('ClientPath')));
