5c2d00c497d6a6e8189c99cad100a2d052d90f7d
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-09-16 19:29:15 +0300

    feat: add interval for logger

--- server/logger/logger.js
+++ server/logger/logger.js
@@ -1,9 +1,24 @@
-const opts = {
-    logFilePath:'./logger/info.log',
-    timestampFormat:'YYYY-MM-DD HH:mm:ss.SSS'
-};
-import  SimpleNodeLogger from 'simple-node-logger'
-const logger=SimpleNodeLogger.createSimpleFileLogger('logger/info.log')
+// const opts = {
+//     logFilePath: './logger/info.log',
+//     timestampFormat: 'YYYY-MM-DD HH:mm:ss.SSS'
+// };
+import SimpleNodeLogger from 'simple-node-logger'
+
+let logger = SimpleNodeLogger.createRollingFileLogger({
+    logDirectory: './logger',
+    dateFormat: 'DD.MM.YYYY',
+    fileNamePattern: 'info_<DATE>.log',
+})
 //const log = SimpleNodeLogger.createSimpleLogger(opts);
+let now = new Date()
+
+
+// setInterval(() => {
+//     let y = now.getFullYear()
+//     let m = now.getMonth()
+//     let d = now.getDate()
+//
+//     logger = SimpleNodeLogger.createSimpleFileLogger(`logger/info${y}`+`_`+`${m}`+`_`+`${d}`+`.log`);
+// }, 2000);
 
 export default logger
