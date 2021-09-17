39c7f3cbfadc543b1a0cd5d753e22ce5d5652eb7
Kalinichenko <aleks.kalinichenko989@gmail.com>
2021-09-17 18:46:18 +0300

    feat: add test connect for mongo add test connect for mongo in index.js

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,31 @@
 import app from "./server.js"
 import logger from './src/logger/logger.js';
+import db from 'mongodb'
+const MongoClient =db.MongoClient
 
-app.listen(5400, () => {
-    logger.info('Server running');
 
+let dbTest
+
+app.get('/users', (req, res) => {
+    dbTest.collection('users').find().toArray((err, docs) => {
+        if (err) {
+            console.log(err)
+            return res.sendStatus(500)
+        } else {
+            res.send(docs)
+        }
+    })
 })
+
+MongoClient.connect('mongodb+srv://XiLLeR:Kalina270698@cluster0.qckeb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', function (err, database) {
+    if (err) {
+        return console.log(err)
+    }
+     dbTest = database.db('test')
+    app.listen(5400, () => {
+        logger.info('Server running');
+
+    })
+})
+
+
--- server/routes/__tests__/auth.test.js
+++ server/routes/__tests__/auth.test.js
@@ -1,6 +1,5 @@
-const request = require('supertest')
-const app = require('../../server')
-const {response} = require("express");
+import request from 'supertest'
+import app from '../../server.js'
 
 it('gallery for uploadImage 200', async () => {
     const res = await request(app).post('/uploadImage')
